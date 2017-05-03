9f91ed47b18b866148c81f3a565b46d1abc4c966
NicoleLane <nlane2@ithaca.edu>
2017-05-03 13:55:39 -0400

    working 3d scatter plot because it's cool

--- visualize.py
+++ visualize.py
@@ -1,6 +1,7 @@
 import plotly as plotly
 import plotly.graph_objs as go
 from plotly.graph_objs import *
+import numpy as np
 
 
 from random import *
@@ -48,6 +49,68 @@ def make_heatmap (cosine_list):
 
     trace = go.Heatmap(z=cosine_list,  # lists of z values by row
                        x=movements, y=movements, colorscale="Jet")
+
     data = [trace]
-    plotly.offline.plot(data)
+    make_scatterplot(cosine_list, movements)
+    plotly.offline.plot(data, filename='heatmap.html')
+
+    return
+
+
+def make_scatterplot (cosine_list, movement_list):
+    print("creating plotly scatterplot...")
+    movements = []
+    queries = []
+    x = list()
+    y = list()
+    z = list()
+
+    path = "data/"
+    item_path = path + "item/"
+    file_path = os.path.dirname(item_path)
+    for file in os.listdir(file_path):
+        item_file = open(item_path + file, "r")
+        for i, line in enumerate(item_file.readlines()):
+            movements.append(line.strip("\n"))
+
+
+    traces = []
+    for i in range (0, 84): #go through movement list, where i is docID of query
+        x.clear()
+        y.clear()
+        z.clear()
+        for j in range (0,84): #j is docID of comparison docs
+            x.append(movements[i])
+            y.append(movements[j])
+            if cosine_list[j][i] < 0.99: #if they aren't the same document
+                z.append(cosine_list[j][i])
+            else:
+                z.append(0)
+
+        trace = Scatter3d(
+            x=x[:],
+            y=y[:],
+            z=z[:],
+            mode='markers',
+            marker=dict(
+                size=5,
+                colorscale='Viridis',  # choose a colorscale
+                opacity=0.8
+            )
+        )
+
+        traces.append(trace)
+    layout = go.Layout(
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        )
+    )
+
+    data = Data(traces)
+
+    fig = Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename='scatter_plot.html')
     return
