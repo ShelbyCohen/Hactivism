e3f4ca0b6f6b147c111ba51fdef14aa277e50213
Shelby Cohen <s.cohen131@gmail.com>
2017-05-04 20:27:31 -0400

    Scatter and new data

--- visualize.py
+++ visualize.py
@@ -49,5 +49,69 @@ def make_heatmap (cosine_list):
     trace = go.Heatmap(z=cosine_list,  # lists of z values by row
                        x=movements, y=movements, colorscale="Jet")
     data = [trace]
+
+    make_scatterplot(cosine_list, movements)
     plotly.offline.plot(data)
+
+
+    return
+
+
+def make_scatterplot (cosine_list, movement_list):
+    print("creating plotly scatterplot...")
+    movements = []
+    queries = []
+    x = list()
+    y = list()
+    z = list()
+
+    path = "data/"
+    item_path = path + "item/"
+    file_path = os.path.dirname(item_path)
+    for file in os.listdir(file_path):
+        item_file = open(item_path + file, "r")
+        for i, line in enumerate(item_file.readlines()):
+            movements.append(line.strip("\n"))
+
+
+    traces = []
+    for i in range (0, 108): #go through movement list, where i is docID of query
+        x.clear()
+        y.clear()
+        z.clear()
+        for j in range (0,108): #j is docID of comparison docs
+            x.append(movements[i])
+            y.append(movements[j])
+            if cosine_list[j][i] < 0.99: #if they aren't the same document
+                z.append(cosine_list[j][i])
+            else:
+                z.append(0)
+
+        trace = Scatter3d(
+            name=movements[i],
+            x=x[:],
+            y=y[:],
+            z=z[:],
+            mode='markers',
+            marker=dict(
+                size=5,
+                colorscale="Jet",  # choose a colorscale
+                opacity=0.8
+            )
+        )
+
+        traces.append(trace)
+    layout = go.Layout(
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        )
+    )
+
+    data = Data(traces)
+
+    fig = Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename='scatter_plot.html')
     return
